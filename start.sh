c5e582eeca8dcb877cea6a4155b62ee1842a2eb7
BlackGabru <123745226+BlackGabru@users.noreply.github.com>
2023-01-28 00:30:05 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/PlayerX/Shortner-Converter-Bot-V2.git /Shortner-Converter-Bot-V2
-cd /Shortner-Converter-Bot-V2
+git clone https://github.com/PlayerX/Player-X-bot.git /Player-X-bot
+cd /Player-X-bot
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
