e562a09af983e573c6efca1d328f053817ef7d97
BlackGabru <123745226+BlackGabru@users.noreply.github.com>
2023-01-28 15:58:15 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo...."
-git clone https://github.com/EagleIX/Shortner-Converter-Bot-V2.git /Shortner-Converter-Bot-V2
+git clone https://github.com/BlackGabru/Shortner-Converter-Bot-V2.git /Shortner-Converter-Bot-V2
 cd /Shortner-Converter-Bot-V2
 pip3 install -r requirements.txt
 echo "Starting Bot...."
