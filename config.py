062e6b9758f27ef5da36ec0eb602707144ea4b9a
BlackGabru <123745226+BlackGabru@users.noreply.github.com>
2023-01-30 16:03:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ DATABASE_NAME = os.environ.get("DATABASE_NAME", "playerxdatabase")
 DATABASE_URL = os.getenv("DATABASE_URL", "mongodb+srv://Aadarsh:Aadarsh07@cluster0.qllzcuy.mongodb.net/?retryWrites=true&w=majority") 
 OWNER_ID =  int(os.environ.get("OWNER_ID", "1382643117")) 
 ADMINS.append(OWNER_ID) if OWNER_ID not in ADMINS else []
-ADMINS.append(Id Owned Id)
+ADMINS.append(Eagleix)
 #  Optionnal variables
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001803407923")) 
 UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "PlayerXLinks") # For Force Subscription
